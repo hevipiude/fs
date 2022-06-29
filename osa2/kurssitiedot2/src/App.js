a161821aa95fe9e618296aafd0431fe312a7bd9d
hevipiude <veerapir@hotmail.com>
2022-06-29 12:07:56 +0300

    2.1

---
+++ osa2/kurssitiedot2/src/App.js
@@ -0,0 +1,80 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+const Header = ({ header }) => {
+  return (
+    <div>
+      <h1> {header} </h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      {name} {exercises}
+    </div>
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+/*const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}*/
+
+export default App
