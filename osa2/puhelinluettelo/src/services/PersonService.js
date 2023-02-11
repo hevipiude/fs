a910973a2806f0caba5c038c431951b8e7d630d1
hevipiude <veerapir@hotmail.com>
2023-02-11 19:58:37 +0200

    removed extra logs

--- osa2/puhelinluettelo/src/services/PersonService.js
+++ osa2/puhelinluettelo/src/services/PersonService.js
@@ -3,26 +3,22 @@ const baseUrl = '/api/persons'
 
 const getPersons = async () => {
   const request = axios.get(baseUrl)
-  console.log(request)
   return request.then((response) => response.data)
 }
 
 const addPerson = async (newObject) => {
   const request = axios.post(baseUrl, newObject)
-  console.log(request)
   return request.then((response) => response.data)
 }
 
 const updatePerson = async (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   console.log('Payload:', newObject)
-  console.log(request)
   return request.then((response) => response.data)
 }
 
 const removePerson = async (id) => {
   const request = axios.delete(`${baseUrl}/${id}`)
-  console.log(request)
   return request.then((response) => response.data)
 }
 
